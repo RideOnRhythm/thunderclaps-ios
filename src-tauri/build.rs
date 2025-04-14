5d40e08613b0d1429bd4caadcdde7ca733cef282
Vancord <107351878+RideOnRhythm@users.noreply.github.com>
2025-04-14 22:06:23 +0800

    Update build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,17 +1,11 @@
-use std::env;
-use std::path::PathBuf;
-
 fn main() {
-    let cwd: PathBuf = env::current_dir().expect("Failed to get current directory");
-    panic!("Current working directory: {}", cwd.display());
-    
-    println!("cargo:rustc-link-search=framework=/..");
-    println!("cargo:rustc-link-lib=framework=TensoridkFlowLiteC");
+    println!("cargo:rustc-link-search=framework=/Users/runner/work/thunderclaps-ios/thunderclaps-ios");
+    println!("cargo:rustc-link-lib=framework=TensorFlowLiteC");
 
-    println!("cargo:rerun-if-changed=/../TensorFlowLiteC.framework/Headers/c_api.h");
+    println!("cargo:rerun-if-changed=/Users/runner/work/thunderclaps-ios/thunderclaps-ios/TensorFlowLiteC.framework/Headers/c_api.h");
 
     let bindings = bindgen::Builder::default()
-        .header("/Users/runner/work/thunderclaps-ios/thunderclaps-ios/src-tauri/TensorFlowLiteC.framework/Headers/c_api.h")
+        .header("/Users/runner/work/thunderclaps-ios/thunderclaps-ios/TensorFlowLiteC.framework/Headers/c_api.h")
         .generate()
         .expect("Unable to generate bindings");
 
